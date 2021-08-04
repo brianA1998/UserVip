df12c49b252acd4158fb578ca7bfd54b56ffbd82
brianA1998 <brian.alaniz@hotmail.com>
2021-08-04 19:49:40 -0300

    Desarrollo de metodo para guardar nombre de usuario

--- app/src/main/java/com/example/uservip/Prefences.kt
+++ app/src/main/java/com/example/uservip/Prefences.kt
@@ -3,6 +3,20 @@ package com.example.uservip
 import android.content.Context
 
 class Prefences(val context: Context) {
+
+    //Constantes
     val SHARED_NAME = "Mydtb"
-    val storage = context.getSharedPreferences(SHARED_NAME,0)
+    val SHARED_USER_NAME = "username"
+
+
+    val storage = context.getSharedPreferences(SHARED_NAME, 0)
+
+
+    /**
+     * Permite guardar nombre de usuario
+     */
+    fun saveName(name: String) {
+        storage.edit().putString(SHARED_USER_NAME, name).apply()
+    }
+
 }
