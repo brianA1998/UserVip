a12ae24dc93cd02fb26ec8b8ee01ea03a67107b4
brianA1998 <brian.alaniz@hotmail.com>
2021-08-04 19:12:16 -0300

    Agregando clasesnuevas y corrigiendo error de importancion de componente de xml

--- app/src/main/java/com/example/uservip/MainActivity.kt
+++ app/src/main/java/com/example/uservip/MainActivity.kt
@@ -1,11 +1,29 @@
 package com.example.uservip
-
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initUI()
     }
+
+    /**
+     * Captura listener del button
+     */
+    fun initUI(){
+        buttonContinue.setOnClickListener { accessToDetail() }
+    }
+
+    fun accessToDetail(){
+        if(editTextName.text.toString().isNotEmpty()){
+
+        }else{
+
+        }
+    }
+
+
 }
---
+++ app/src/main/java/com/example/uservip/Preferences.java
@@ -0,0 +1,4 @@
+package com.example.uservip;
+
+public class Preferences {
+}
